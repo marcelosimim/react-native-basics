b131e215d9fbfc1af9375c664b73234951b3cd41
Marcelo Simim <54868944+marcelosimim@users.noreply.github.com>
2023-07-21 20:33:41 -0300

    flat list with key

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button, View, StyleSheet, TextInput, Text, ScrollView } from 'react-native';
+import { Button, View, StyleSheet, TextInput, Text, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('')
@@ -12,7 +12,7 @@ export default function App() {
   function addGoalHandler() { 
     setCourseGoals(currentCourseGoals =>  [
       ...currentCourseGoals, 
-      enteredGoalText
+      {text: enteredGoalText, key: Math.random().toString()}
     ])
   }
 
@@ -23,15 +23,13 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView alwaysBounceVertical={false}>
-          {courseGoals.map((goal) => (
-            <View key={goal} style={styles.goalItem}>
-              <Text style={styles.goalText}>
-              {goal}
-              </Text>
+        <FlatList data={courseGoals} renderItem={(itemData) => {
+          return (
+            <View style={styles.goalItem}>
+              <Text style={styles.goalText}>{itemData.item.text}</Text>
             </View>
-          ))}
-        </ScrollView>
+          )
+        }} alwaysBounceVertical={false} />
       </View>
     </View>
   )
